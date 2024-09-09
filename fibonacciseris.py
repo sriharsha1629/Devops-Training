bed218f7e95fd5eb9e24c3527ed9e2252ceca7d4
Sriharsha <98337856+sriharsha1629@users.noreply.github.com>
2024-09-09 09:30:27 +0000

    Update fibonacciseris.py

--- fibonacciseris.py
+++ fibonacciseris.py
@@ -1,22 +1,20 @@
-def  fibonacci(n):
+fibonaci=[0, 1, 1, 2, 3, 5, 8, 13,21,34,55,89,144,233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28567, 46368, 75025, 121393, 196418, 317811]
+def process(*number):
+    numbers=list(number)
+    for i in range(len(number)):
+        if number[i] in fibonaci:
+            print(f"---{numbers[i]} is a fibonaci number.---")
+        else:
+            print(f"---{numbers[i]} is not valid fibonaci number.---")
 
-	if n == 1  or n == 0:
-
-		return n;
-
-	else:
-
-		return fibonacci(n-2) + fibonacci(n - 1)
 
 
-numero = int(input("ingrese un numero entero  positivo: "))
 
-if numero < 0:
-	print("Numero no valido")
+while True:
+    choice=input("Do you want to START/KILL Press(S/K): ")
+    if  choice == 's':
+        
 
-i = 0
-
-print("Secesion de fibonacci: ")
-
-for i in range(0, numero):
-	print(fibonacci(i))
+        process(int(input("Enter any Desired Number: ")),int(input("Enter any Desired Number: ")))
+    else:
+        exit()
